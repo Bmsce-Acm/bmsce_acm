d2d53d294343f4c3ea2f045c73db0c42435041f0
Siddarth72 <72@siddarth.com>
2022-09-29 20:59:47 +0530

    server setup

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,18 @@
-const app = require('./app');
+// const app = require('./app');
+const path = require('path');
+const express = require('express');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+require("dotenv").config({ path: "backend/config/config.env" });
+
+const app = express();
+// if (process.env.NODE_ENV !== "production") {
+// 	require("dotenv").config({ path: "backend/config/config.env" });
+// }
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // ! Handle UnCaught Exceptions
 process.on("uncaughtException", (err) => {
@@ -7,8 +21,15 @@ process.on("uncaughtException", (err) => {
 	process.exit(1);
 });
 
-if (process.env.NODE_ENV !== "production") {
-	require("dotenv").config({ path: "backend/config/config.env" });
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'frontend/build')))
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+	app.get('/', (req, res) => {
+		res.send('Server is running')
+	})
 }
 
 const server = app.listen(process.env.PORT, () => {
